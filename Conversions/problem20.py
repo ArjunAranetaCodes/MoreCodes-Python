777fa0627a30190da452d7838b62e5994ac80d0b
Araneta <arjunaraneta@gmail.com>
2018-03-20 09:06:55 +0800

    2018 Updates

---
+++ Conversions/problem20.py
@@ -0,0 +1,8 @@
+#Problem 20: Write a program that converts a number to its 
+#corresponding month (e.g. 1 = January).
+index = 1
+month = ["January", "February", "March", "April",
+"May", "June", "July", "August",
+"September", "October", "November", "December"]
+
+print(month[index - 1])
